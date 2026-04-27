052c27b2708e95a7a330d7514e15e4836356a51c
robertceron359-star <robertceron359@gmail.com>
2026-04-26 22:07:11 -0500

    refactor: reestructuración de arquitectura base a Python MVC v1.0.0
    Se eliminan artefactos web y se inicializan directorios para modelos, vistas y controladores, ademas de archivos JSON para guardar y cargar los datos

---
+++ src/views/login_view.py
@@ -0,0 +1,5 @@
+import sys
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+class LoginView(QWidget):
+    def __init__(self):
+        super().__init__(); self.setWindowTitle('Libros-Xpress')
